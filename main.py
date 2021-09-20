77b331fe9f132938a6542391f0d2c64a4d2f9db3
svingel <office@kostrzewa-consulting.com>
2021-09-20 15:07:17 +0200

    Homework 1

---
+++ main.py
@@ -0,0 +1,7 @@
+# Betroot Academy - Intro To Tech - Week 4: Python
+# Homework Day 1
+
+print('Welcome To MyFriends 1.0!')
+
+print('''\n#####\n#\t#\n#\t#\n#\t#\n#####''')
+print('''\n#\t#\n#\t#\n#####\n#\t#\n#\t#''')
